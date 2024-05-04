92e3472eec6dcc366b3221d071c015b304953178
Ag <agata.a.rajska@gmail.com>
2024-05-04 11:23:50 +0200

    first attempt

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import socket
 
+def generate_response():
+    response = [0 for _ in range(12)] #it's always 12 bytes long
+    response[0] = (1234 >> 8) & 255 #packet ID in big-endian format; shifts the bits of 1234 right by 8 and masks with 255
+    response[1] = 1234 & 255 #mask with 255 to get the low byte
+    response[2] = 128 #corresponds to 10000000; qr bit = 1 now as expected
+    return bytes(response)
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,7 +15,8 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
     
-            response = b""
+            response = generate_response()
+            print(response)
     
             udp_socket.sendto(response, source)
         except Exception as e:
